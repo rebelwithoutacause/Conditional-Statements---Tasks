158c2d670bac8b78c883b9ef3ae3869f315c36e0
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-01 00:46:03 +0200

    Add files via upload

---
+++ 06.ProductOfThreeNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int num3 = int.Parse(Console.ReadLine());
+
+        
+        if (num1 == 0 || num2 == 0 || num3 == 0)
+        {
+            Console.WriteLine("zero");
+            return;  
+        }
+
+        
+        int negativeCount = 0;
+
+        if (num1 < 0) negativeCount++;
+        if (num2 < 0) negativeCount++;
+        if (num3 < 0) negativeCount++;
+
+        
+        if (negativeCount % 2 != 0)
+        {
+            Console.WriteLine("negative");
+        }
+        else
+        {
+            Console.WriteLine("positive");
+        }
+    }
+}
---
+++ 07.WorkingHours/Program.cs
@@ -0,0 +1,12 @@
+﻿int hourOfTheDay = int.Parse(Console.ReadLine());
+string dayOfTheWeek = Console.ReadLine();
+
+if (hourOfTheDay >= 10 && hourOfTheDay <= 18 &&
+    (dayOfTheWeek == "Monday" || dayOfTheWeek == "Tuesday" || dayOfTheWeek == "Wednesday" || dayOfTheWeek == "Thursday" || dayOfTheWeek == "Friday" || dayOfTheWeek == "Saturday"))
+{
+    Console.WriteLine("open");
+}
+else
+{
+    Console.WriteLine("closed");
+}
